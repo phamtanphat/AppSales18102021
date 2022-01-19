378bf6e1c681db74d56775f9d901085b5f45b953
phamtanphat <phatdroid94@gmail.com>
2022-01-19 18:56:37 +0700

    inject

--- app/src/main/java/com/example/appsales18102021/MainComponent.java
+++ app/src/main/java/com/example/appsales18102021/MainComponent.java
@@ -1,4 +1,9 @@
 package com.example.appsales18102021;
 
+import dagger.Component;
+
+@Component(modules = CarModule.class)
 public interface MainComponent {
+
+   void injectMain(MainActivity mainActivity);
 }
