e98c22fb19f2f79dd72764d4997d04d42d4bcb24
phamtanphat <phatdroid94@gmail.com>
2022-01-21 18:40:46 +0700

    setup project

--- app/src/main/java/com/example/appsales18102021/MyApplication.java
+++ app/src/main/java/com/example/appsales18102021/MyApplication.java
@@ -1,12 +1,4 @@
 package com.example.appsales18102021;
 
-import dagger.android.AndroidInjector;
-import dagger.android.support.DaggerApplication;
-
-public class MyApplication extends DaggerApplication {
-
-    @Override
-    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return DaggerMainComponent.builder().context(this).build();
-    }
+public class MyApplication {
 }
---
+++ app/src/main/java/com/example/appsales18102021/presentation/features/login/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.appsales18102021.presentation.features.login;
+
+import android.os.Bundle;
+
+import com.example.appsales18102021.R;
+
+import dagger.android.support.DaggerAppCompatActivity;
+
+public class LoginActivity extends DaggerAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
