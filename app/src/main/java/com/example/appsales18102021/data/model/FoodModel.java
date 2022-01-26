25453aa87f193b37e9a428e566c985edf8acf26e
phamtanphat <phatdroid94@gmail.com>
2022-01-26 18:37:36 +0700

    food model , image model

---
+++ app/src/main/java/com/example/appsales18102021/data/model/FoodModel.java
@@ -0,0 +1,91 @@
+package com.example.appsales18102021.data.model;
+
+import android.media.Image;
+
+import java.util.List;
+
+public class FoodModel {
+
+    private String foodId;
+    private String foodName;
+    private List<ImageModel> images = null;
+    private String description;
+    private Integer price;
+    private String cateId;
+    private String cateName;
+    private String createdAt;
+    private String updatedAt;
+
+    public String getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(String foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public List<ImageModel> getImages() {
+        return images;
+    }
+
+    public void setImages(List<ImageModel> images) {
+        this.images = images;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getCateId() {
+        return cateId;
+    }
+
+    public void setCateId(String cateId) {
+        this.cateId = cateId;
+    }
+
+    public String getCateName() {
+        return cateName;
+    }
+
+    public void setCateName(String cateName) {
+        this.cateName = cateName;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
---
+++ app/src/main/java/com/example/appsales18102021/data/model/ImageModel.java
@@ -0,0 +1,16 @@
+package com.example.appsales18102021.data.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ImageModel {
+    private String imageUrl;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
